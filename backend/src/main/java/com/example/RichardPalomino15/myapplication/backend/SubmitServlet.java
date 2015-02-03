a647398cfe932beae41ee67d5d1298ddce96d9da
rmpalomino <Richard.m.palomino.15@dartmouth.edu>
2015-02-03 17:42:04 -0500

    Initial add of QuerySevlet.

--- backend/src/main/java/com/example/RichardPalomino15/myapplication/backend/SubmitServlet.java
+++ backend/src/main/java/com/example/RichardPalomino15/myapplication/backend/SubmitServlet.java
@@ -1,7 +1,26 @@
 package com.example.RichardPalomino15.myapplication.backend;
 
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 /**
  * Created by Richard Palomino 15 on 2/3/2015.
  */
-public class SubmitServlet {
+public class SubmitServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
 }
+
+
