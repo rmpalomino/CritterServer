bf6f79be3a901abf9a261db565dfc8e235072a01
rmpalomino <Richard.m.palomino.15@dartmouth.edu>
2015-02-09 19:37:59 -0500

    Added JSON contract for Game state.

---
+++ backend/src/main/java/com/example/RichardPalomino15/myapplication/backend/GameStateJSONContract.java
@@ -0,0 +1,17 @@
+package com.example.RichardPalomino15.myapplication.backend;
+
+/**
+ * Created by RichardPalomino on 2/9/2015.
+ */
+public class GameStateJSONContract {
+
+        public static final String TIME_ELAPSED  = "timeElapsed";
+        public static final String TURNS_ELAPSED  = "turnsElapsed";
+        public static final String ENTITIES  = "entities";
+        public static final String PLAYER  = "player";
+        public static final String POSITION = "position";
+        public static final String ACTIONS = "actions";
+        public static final String ACTION_TYPE = "myAction";
+        public static final String ACTION_DETAILS = "myOrders";
+
+}
